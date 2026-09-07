bbaac796d3dbe1575013157cc44f61c7fa2859ca
Yuval Herman <yuvalherman99@gmail.com>
2026-09-07 04:51:32 +0300

    test for correct value as well as correct outcome for int options parsing

--- test_src/test_int.c
+++ test_src/test_int.c
@@ -74,6 +74,70 @@ static void verify_u64(const char *input, uint64_t expected, int base) {
   }
 }
 
+static void verify_i32_options(const char *input, int32_t expected, int base, ffc_parse_options options) {
+  size_t len = strlen(input);
+  char* pend = (char*)(input + len);
+  int32_t out = 0;
+  ffc_result r = ffc_from_chars_i32_options(input, pend, base, &out, options);
+  if (r.outcome != FFC_OUTCOME_OK) {
+    fprintf(stderr, "FAIL i32 options parse \"%s\" base %d: unexpected outcome %d\n",
+            input, base, r.outcome);
+    FAILS++;
+  } else if (out != expected) {
+    fprintf(stderr, "FAIL i32 options \"%s\" base %d: got %d, expected %d\n",
+            input, base, (int)out, (int)expected);
+    FAILS++;
+  }
+}
+
+static void verify_u32_options(const char *input, uint32_t expected, int base, ffc_parse_options options) {
+  size_t len = strlen(input);
+  char* pend = (char*)(input + len);
+  uint32_t out = 0;
+  ffc_result r = ffc_from_chars_u32_options(input, pend, base, &out, options);
+  if (r.outcome != FFC_OUTCOME_OK) {
+    fprintf(stderr, "FAIL u32 options parse \"%s\" base %d: unexpected outcome %d\n",
+            input, base, r.outcome);
+    FAILS++;
+  } else if (out != expected) {
+    fprintf(stderr, "FAIL u32 options \"%s\" base %d: got %u, expected %u\n",
+            input, base, (unsigned)out, (unsigned)expected);
+    FAILS++;
+  }
+}
+
+static void verify_i64_options(const char *input, int64_t expected, int base, ffc_parse_options options) {
+  size_t len = strlen(input);
+  char* pend = (char*)(input + len);
+  int64_t out = 0;
+  ffc_result r = ffc_from_chars_i64_options(input, pend, base, &out, options);
+  if (r.outcome != FFC_OUTCOME_OK) {
+    fprintf(stderr, "FAIL i64 options parse \"%s\" base %d: unexpected outcome %d\n",
+            input, base, r.outcome);
+    FAILS++;
+  } else if (out != expected) {
+    fprintf(stderr, "FAIL i64 options \"%s\" base %d: got %lld, expected %lld\n",
+            input, base, (long long)out, (long long)expected);
+    FAILS++;
+  }
+}
+
+static void verify_u64_options(const char *input, uint64_t expected, int base, ffc_parse_options options) {
+  size_t len = strlen(input);
+  char* pend = (char*)(input + len);
+  uint64_t out = 0;
+  ffc_result r = ffc_from_chars_u64_options(input, pend, base, &out, options);
+  if (r.outcome != FFC_OUTCOME_OK) {
+    fprintf(stderr, "FAIL u64 options parse \"%s\" base %d: unexpected outcome %d\n",
+            input, base, r.outcome);
+    FAILS++;
+  } else if (out != expected) {
+    fprintf(stderr, "FAIL u64 options \"%s\" base %d: got %llu, expected %llu\n",
+            input, base, (unsigned long long)out, (unsigned long long)expected);
+    FAILS++;
+  }
+}
+
 static void expect_outcome_i32(const char *input, int base, ffc_outcome expected_outcome) {
   size_t len = strlen(input);
   int32_t out = 0;
@@ -111,54 +175,6 @@ static void expect_outcome_u64(const char *input, int base, ffc_outcome expected
   size_t len = strlen(input);
   uint64_t out = 0;
   ffc_result r = ffc_parse_u64(len, input, base, &out);
-  if (r.outcome != expected_outcome) {
-    fprintf(stderr, "FAIL u64 \"%s\" base %d: got outcome %d, expected %d\n",
-            input, base, r.outcome, expected_outcome);
-    FAILS++;
-  }
-}
-
-static void expect_outcome_i32_options(const char *input, int base, ffc_parse_options options, ffc_outcome expected_outcome) {
-  size_t len = strlen(input);
-  char *pend = (char*)(input + len);
-  int32_t out = 0;
-  ffc_result r = ffc_from_chars_i32_options(input, pend, base, &out, options);
-  if (r.outcome != expected_outcome) {
-    fprintf(stderr, "FAIL i32 \"%s\" base %d: got outcome %d, expected %d\n",
-            input, base, r.outcome, expected_outcome);
-    FAILS++;
-  }
-}
-
-static void expect_outcome_u32_options(const char *input, int base, ffc_parse_options options, ffc_outcome expected_outcome) {
-  size_t len = strlen(input);
-  char *pend = (char*)(input + len);
-  uint32_t out = 0;
-  ffc_result r = ffc_from_chars_u32_options(input, pend, base, &out, options);
-  if (r.outcome != expected_outcome) {
-    fprintf(stderr, "FAIL u32 \"%s\" base %d: got outcome %d, expected %d\n",
-            input, base, r.outcome, expected_outcome);
-    FAILS++;
-  }
-}
-
-static void expect_outcome_i64_options(const char *input, int base, ffc_parse_options options, ffc_outcome expected_outcome) {
-  size_t len = strlen(input);
-  char *pend = (char*)(input + len);
-  int64_t out = 0;
-  ffc_result r = ffc_from_chars_i64_options(input, pend, base, &out, options);
-  if (r.outcome != expected_outcome) {
-    fprintf(stderr, "FAIL i64 \"%s\" base %d: got outcome %d, expected %d\n",
-            input, base, r.outcome, expected_outcome);
-    FAILS++;
-  }
-}
-
-static void expect_outcome_u64_options(const char *input, int base, ffc_parse_options options, ffc_outcome expected_outcome) {
-  size_t len = strlen(input);
-  char *pend = (char*)(input + len);
-  uint64_t out = 0;
-  ffc_result r = ffc_from_chars_u64_options(input, pend, base, &out, options);
   if (r.outcome != expected_outcome) {
     fprintf(stderr, "FAIL u64 \"%s\" base %d: got outcome %d, expected %d\n",
             input, base, r.outcome, expected_outcome);
@@ -245,39 +261,37 @@ static void test_u64_invalid(void) {
 /* -- parse options ------------------------------------------------- */
 
 static void test_i32_options(void) {
-  const char *cases[] = { "+50", " 50", "-50" };
-  ffc_parse_options options = {
-    .format = FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS | FFC_FORMAT_FLAG_SKIP_WHITE_SPACE
-  };
-  for (size_t i = 0; i < sizeof(cases)/sizeof(*cases); i++)
-    expect_outcome_i32_options(cases[i], 10, options, FFC_OUTCOME_OK);
+  ffc_parse_options options = ffc_parse_options_default();
+  options.format |= FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS;
+  options.format |= FFC_FORMAT_FLAG_SKIP_WHITE_SPACE;
+  verify_i32_options("+50", 50, 10, options);
+  verify_i32_options(" 50", 50, 10, options);
+  verify_i32_options("-50", -50, 10, options);
 }
 
 static void test_u32_options(void) {
-  const char *cases[] = { "+50", " 50" };
-  ffc_parse_options options = {
-    .format = FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS | FFC_FORMAT_FLAG_SKIP_WHITE_SPACE
-  };
-  for (size_t i = 0; i < sizeof(cases)/sizeof(*cases); i++)
-    expect_outcome_u32_options(cases[i], 10, options, FFC_OUTCOME_OK);
+  ffc_parse_options options = ffc_parse_options_default();
+  options.format |= FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS;
+  options.format |= FFC_FORMAT_FLAG_SKIP_WHITE_SPACE;
+  verify_u32_options("+50", 50, 10, options);
+  verify_u32_options(" 50", 50, 10, options);
 }
 
 static void test_i64_options(void) {
-  const char *cases[] = { "+50", " 50", "-50" };
-  ffc_parse_options options = {
-    .format = FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS | FFC_FORMAT_FLAG_SKIP_WHITE_SPACE
-  };
-  for (size_t i = 0; i < sizeof(cases)/sizeof(*cases); i++)
-    expect_outcome_i64_options(cases[i], 10, options, FFC_OUTCOME_OK);
+  ffc_parse_options options = ffc_parse_options_default();
+  options.format |= FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS;
+  options.format |= FFC_FORMAT_FLAG_SKIP_WHITE_SPACE;
+  verify_i64_options("+50", 50, 10, options);
+  verify_i64_options(" 50", 50, 10, options);
+  verify_i64_options("-50", -50, 10, options);
 }
 
 static void test_u64_options(void) {
-  const char *cases[] = { "+50", " 50" };
-  ffc_parse_options options = {
-    .format = FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS | FFC_FORMAT_FLAG_SKIP_WHITE_SPACE
-  };
-  for (size_t i = 0; i < sizeof(cases)/sizeof(*cases); i++)
-    expect_outcome_u64_options(cases[i], 10, options, FFC_OUTCOME_OK);
+  ffc_parse_options options = ffc_parse_options_default();
+  options.format |= FFC_FORMAT_FLAG_ALLOW_LEADING_PLUS;
+  options.format |= FFC_FORMAT_FLAG_SKIP_WHITE_SPACE;
+  verify_u64_options("+50", 50, 10, options);
+  verify_u64_options(" 50", 50, 10, options);
 }
 
 /* -- out of range (decimal) ---------------------------------------- */
